a7063b8df3427f662e349ee3ff9b9ec74e6ac8e1
arminbayat <arminbayat@yahoo.com>
2023-10-13 12:09:25 +0330

    edit function

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -29,7 +29,7 @@ const MobilteItem: React.FC<MobilteItemProbs> = ({
     className={clsx(
         `group flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100`, active && 'bg-gray-100 text-black'
     )}
-    onClick={onClick}
+    onClick={hanldleClick}
     href={href}>
       <Icon className ='h-6 w-6'/>
     </Link>
