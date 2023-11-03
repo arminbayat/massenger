7207054fdd85c0ba6e4f7f9830170152f2c011b8
arminbayat <arminbayat@yahoo.com>
2023-11-03 12:32:12 +0330

    fix buttom problem

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,16 @@
-"use client";
 import clsx from "clsx";
-import { ReactNode } from "react";
 
-interface ButtonProbs {
-  type: "button" | "submit" | "reset" | undefined;
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
   fullWidth?: boolean;
-  children?: ReactNode;
+  children?: React.ReactNode;
   onClick?: () => void;
   secondary?: boolean;
   danger?: boolean;
   disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProbs> = ({
+const Button: React.FC<ButtonProps> = ({
   type = "button",
   fullWidth,
   children,
@@ -28,22 +26,22 @@ const Button: React.FC<ButtonProbs> = ({
       disabled={disabled}
       className={clsx(
         `
-  flex
-  justify-center
-  rounded-md
-  px-3
-  py-2  
-  text-sm
-  font-semibold
-  focus-visibe:outline
-  focus-visible:outline-2
-  focus-visible:outline-offset-2
-  `,
+        flex 
+        justify-center 
+        rounded-md 
+        px-3 
+        py-2 
+        text-sm 
+        font-semibold 
+        focus-visible:outline 
+        focus-visible:outline-2 
+        focus-visible:outline-offset-2 
+        `,
         disabled && "opacity-50 cursor-default",
         fullWidth && "w-full",
         secondary ? "text-gray-900" : "text-white",
         danger &&
-          "bg-roos-500 hover:bg-rose-500 focus-visible:outline-rose-600",
+          "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
         !secondary &&
           !danger &&
           "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
